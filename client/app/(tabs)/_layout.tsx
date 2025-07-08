3a3a0cfb94c12e7b33f712f53708b6c395d82665
Kunal Vats <1kunalvats9@gmail.com>
2025-07-09 00:39:35 +0530

    addded admin panel

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,11 +1,27 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { Tabs } from 'expo-router'
+import { Redirect, Tabs } from 'expo-router'
+import { useAuth, useUser } from '@clerk/clerk-expo'
+import {Feather} from "@expo/vector-icons"
 
 const _layout = () => {
+  const { isSignedIn } = useAuth();
+  const { user } = useUser();
+  const isAdmin = user?.publicMetadata?.role === 'admin';
+  if(!isSignedIn){
+    return <Redirect href={"/(auth)"} />
+  }
   return (
     <Tabs>
         <Tabs.Screen name='home' options={{headerShown:false}} />
+        <Tabs.Screen
+                name='admin'
+                options={{
+                    title: 'Admin',
+                    tabBarIcon: ({ color, size }) => <Feather size={size} color={color} name='settings' />,
+                    href: isAdmin ? '/(tabs)/admin' : null,
+                }}
+            />
     </Tabs>
   )
 }
