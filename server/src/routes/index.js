7ec714f614eb79157f2499dca454a7936884c13d
Kunal Vats <1kunalvats9@gmail.com>
2025-07-09 15:35:50 +0530

    Remove Cloudinary Integration from Server

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -3,7 +3,6 @@ import productsRoutes from './products.routes.js';
 import categoriesRoutes from './categories.routes.js';
 import ordersRoutes from './orders.routes.js';
 import usersRoutes from './users.routes.js';
-import uploadRoutes from './upload.routes.js';
 
 const router = express.Router();
 
@@ -21,6 +20,5 @@ router.use('/products', productsRoutes);
 router.use('/categories', categoriesRoutes);
 router.use('/orders', ordersRoutes);
 router.use('/users', usersRoutes);
-router.use('/upload', uploadRoutes);
 
 export default router;
